3ff645f1c13da229305a213bc9b75c552b41803a
Gabriel De Jesus <111870550+Xviert@users.noreply.github.com>
2025-08-24 10:16:52 -0300

    Add files via upload

---
+++ Extremamente Basico/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+        sc.close();
+    }
+}
